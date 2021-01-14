3d85394c36e77310533717eaeb6355a45de848f7
vladosxxx <vladosxxx@gmail.com>
2021-01-14 17:15:00 +0200

    added messeger sender

--- src/components/ChatComponent.js
+++ src/components/ChatComponent.js
@@ -4,7 +4,8 @@ import '../style/remixicon/remixicon.css'
 
 
 function ChatComponent() {
-    const [getMesseges] = useState(
+    const [getText, setText] = useState("");
+    const [getMesseges, setMesseges] = useState(
         [
             {
                 sender: "Юлия Дворецкова",
@@ -17,9 +18,27 @@ function ChatComponent() {
                 time: new Date('2021-01-14T04:24:00'),
                 likes: 1,
                 text: "Равным образом дальнейшее развитие различных форм деятельности способствует подготовке и реализации существенных финансовых условий."
+            },
+            {
+                sender: "Me",
+                time: new Date('2021-01-14T04:24:00'),
+                likes: 1,
+                text: "Равным образом дальнейшее развитие различных форм деятельности способствует подготовке и реализации существенных финансовых условий."
             }
         ]
     )
+    function addMess(textPush)
+    {
+        setMesseges([
+            ...getMesseges, 
+            {
+                sender: "Me",
+                time: new Date(),
+                likes: 0,
+                text: textPush
+        }]);
+        setText("")
+    }
     return (
         <div className="chat">
             <div className="chat-head">
@@ -30,20 +49,22 @@ function ChatComponent() {
                     {getMesseges.map( (mess) => {
                         if(mess.sender === 'Me')
                         {
-                            return <li style={{color: "red"}} key={mess.time}>{mess.text}</li>
+                            return <li className="my-messeges" key={mess.time}><p>{mess.text}</p></li>
                         }
                         else
-                            return <li key={mess.time}>{mess.text}</li>
+                            return <li key={mess.time}><p>{mess.text}</p></li>
                     })}
                 </ul>
             </div>
             <div className="chat-input">
-            <input placeholder="Написать сообщение..."/>
-            <i class="ri-emotion-happy-line"></i>
-            <i class="ri-attachment-2"></i>
-            <div className="button" type='button'>
-                <p>Отправить</p>
-            </div>
+                <form>
+                <input placeholder="Написать сообщение..." value={getText} onChange={e => setText(e.target.value)}/>
+                <div className="button" type='button' onClick={() => addMess(getText)}>
+                        <p>Отправить</p>
+                </div>
+                <i class="ri-emotion-happy-line"></i>
+                <i class="ri-attachment-2"></i>
+                </form>
             </div>
         </div>
     );
